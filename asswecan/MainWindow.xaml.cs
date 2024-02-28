c4d34fd430e59fdf7e76a4d36b8f08972b30af34
Artyom Khlebodarov <tyomajoraly@gmail.com>
2024-02-28 21:59:15 +0300

    WiP: Initial commit

---
+++ asswecan/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace asswecan
+{
+
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            datepicker.SelectedDate = DateTime.Today;
+
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            
+            /*Создать*/
+            DateTime? date = datepicker.SelectedDate;
+            string name = name_for_note.Text;
+            string description = discript_for_note.Text;
+            Note n = new Note(name, description, date);
+            Note.notes.Add(n);
+            zalupa.Text = name;
+            listbox.ItemsSource = Note.notes;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void name_for_note_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void listbox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (listbox.SelectedItem != null)
+                this.Title = (listbox.SelectedItem as Note).getNoteName();
+        }
+
+        private void datepicker_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ asswecan/Note.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asswecan
+{
+    public class Note
+    {
+        public static List<Note> notes = new List<Note>();
+
+        
+
+        // модель заметки
+        public static string noteName { get; set; }
+        public static string noteDescripton { get; set; }
+        public static DateTime? noteDate { get; set; }
+
+        public string getNoteName() { return noteName; }
+        public Note(string nN, string nD, DateTime? nDt)
+        {
+            noteName = nN;
+            noteDescripton = nD;
+            noteDate = nDt;
+        }
+
+    }
+
+}
